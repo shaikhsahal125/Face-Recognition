2930bde70b94de05f871dd08f38f3b5c6deaa381
shaikhsahal125 <shaikhsahal125@gmail.com>
2021-01-03 21:06:47 -0500

    added controller methods and new window, but did not link yet.

--- src/controller/HomePageController.java
+++ src/controller/HomePageController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class HomePageController {
+
+    public void recognizeBtnCtrl(ActionEvent event) {
+        System.out.println("Recognize Btn clicked");
+    }
+
+
+    public void addFaceBtnCtrl(ActionEvent event) {
+        System.out.println("Add face Btn clicked");
+    }
+
 }
