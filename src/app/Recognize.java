385448d6b5155d93e880159625df9cff9972a3ec
shaikhsahal125 <shaikhsahal125@gmail.com>
2021-01-03 19:31:33 -0500

    first page loads fine with background image (Used css styling).

--- src/app/Recognize.java
+++ src/app/Recognize.java
@@ -2,14 +2,23 @@ package app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Recognize extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/view/home_page.fxml"));
 
+        AnchorPane root = loader.load();
 
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Recognize People");
+        stage.setResizable(false);
+        stage.show();
     }
 
     public static void main(String[] args) {
