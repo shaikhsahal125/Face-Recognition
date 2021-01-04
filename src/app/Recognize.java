17bf0639527c6c9805643b02cb7493dac00288a4
shaikhsahal125 <shaikhsahal125@gmail.com>
2021-01-03 19:11:42 -0500

    made first page UI.

---
+++ src/app/Recognize.java
@@ -0,0 +1,18 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+public class Recognize extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
