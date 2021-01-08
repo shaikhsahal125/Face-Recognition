d85ea38a5d35603f20073d2d34713571b5fb1341
shaikhsahal125 <shaikhsahal125@gmail.com>
2021-01-08 10:18:35 -0500

    serialization is done through json format

--- src/model/addFace.py
+++ src/model/addFace.py
@@ -10,6 +10,13 @@ person_name = sys.argv[1]
 addFacePath = sys.argv[2]
 
 # people = People()
+people_dict = {}
+
+with open("src/model/serializeStorage.txt", "r") as file:
+    people_dict = json.load(file)
+    file.close()
+
+print(people_dict)
 
 print(os.getcwd())
 destination = os.getcwd() + "/src/model/images"
@@ -17,5 +24,11 @@ copy(addFacePath, destination)
 # print(destination + "/" + addFacePath.split("/")[-1])
 
 # people.addPerson(Person(person_name, destination + "/" + addFacePath.split("/")[-1]))
+
+people_dict[person_name] = destination
+
+with open("src/model/serializeStorage.txt", "w") as file:
+    json.dump(people_dict, file)
+    file.close()
 
 print("success")
